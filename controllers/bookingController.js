34de5144eff231029a85e23f6478d4cded52df0d
TriciaChimalu <ozobialupatricia@gmail.com>
2026-08-04 17:10:46 -0700

    updated bookingController

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -57,7 +57,8 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].amount / 100;
+  const line_items = await stripe.checkout.sessions.listLineItems(session.id);
+  const price = line_items.data[0].amount_total / 100;
   await Booking.create({ tour, user, price });
 };
 
